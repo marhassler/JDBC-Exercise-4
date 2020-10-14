df44781e3470c3eadeef16a0acdbd974f6e3b5eb
marhassler <marhassler@tsn.at>
2020-10-14 06:56:36 +0200

    commit

--- src/main/java/testdb/App.java
+++ src/main/java/testdb/App.java
@@ -7,10 +7,11 @@ import java.util.ArrayList;
  * Hello world!
  */
 public class App {
+    public static Connection con;
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Test", "root", "1234");
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Test", "root", "1234");
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("select * from Invoices");
             while (rs.next())
@@ -20,8 +21,21 @@ public class App {
             System.out.println(e);
         }
         java.util.Date date2 = new java.util.Date();
-        InvoiceDao invoiceDao = new InvoiceDaoArrayListImpl();
+        InvoiceDao invoiceDaoSQL = new SQLInvoiceDaoIImpl();
+        insertInvoice(date,"bestellung 231",324, (byte) 1);
+        invoiceDaoSQL.updateInvoiceSQL(1,date,"bestellung 1 neu",192, (byte) 1);
+        deleteInvoice(9,connectDatabase());
 
+
+
+
+
+
+
+
+        //ArrayList Implementation
+
+        InvoiceDao invoiceDao = new InvoiceDaoArrayListImpl();
         ArrayList<Invoice> liste = invoiceDao.showAllInvoices();
         for (Invoice i : liste) {
             System.out.println(i);
